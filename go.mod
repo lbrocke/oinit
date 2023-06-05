713e8a3d1c0d36ce1e820ca53a48b4c16dbffaf6
Lukas Brocke <lukas@brocke.net>
2023-06-05 14:42:44 +0200

    Implement add, delete and list command

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module oinit
 
 go 1.20
+
+require (
+	github.com/indigo-dc/liboidcagent-go v0.4.0
+	github.com/stretchr/testify v1.8.3
+)
+
+require (
+	github.com/adrg/xdg v0.4.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/oidc-mytoken/api v0.8.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- oinit.go
+++ oinit.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"oinit/src/dnsutil"
+	"oinit/src/liboinitca"
+	"oinit/src/sshutil"
 	"os"
+	"sort"
+	"strings"
 )
 
 const (
@@ -13,26 +19,140 @@ const (
 
 	USAGE = "Usage:\n" +
 		"\toinit add    <host>[:port] [ca]\tAdd a host managed by oinit.\n" +
-		"\toinit delete <host> [port]\tDelete a host.\n" +
-		"\toinit match  <host> [port]\tCheck whether host is managed by oinit. Used in your ssh config.\n"
+		"\toinit delete <host>[:port]\tDelete a host.\n" +
+		"\toinit list\t\t\tList all hosts managed by oinit.\n"
 )
 
 func handleCommandAdd(args []string) {
-	// todo
+	if len(args) < 1 {
+		fmt.Print(USAGE)
+		return
+	}
+
+	hostport := args[0]
+
+	// Split into host and port, as in some cases special handling is required
+	// (such as in the known hosts file).
+	host, port, err := net.SplitHostPort(hostport)
+	if err != nil {
+		// Assume that hostport string given to net.SplitHostPort() doesn't contain a port
+		// https://groups.google.com/g/golang-nuts/c/KA41Tj9Aabg/m/1NUcxQcoUjwJ
+		host = strings.TrimSpace(hostport)
+		port = "22"
+
+		hostport = net.JoinHostPort(host, port)
+	}
+
+	// Check if host was already added before. This also includes the system-wide
+	// configuration, therefore hosts that were added by the system admin
+	// won't be added to the user config again.
+	if found, err := sshutil.IsManagedHost(hostport); err != nil {
+		LogError("Could not read hosts file: " + err.Error())
+		return
+	} else if found {
+		LogInfo("This host was already added.")
+		return
+	}
+
+	// Determine CA from DNS if not given on command line.
+	var ca string
+	if len(args) >= 2 {
+		ca = args[1]
+	} else {
+		detected, err := dnsutil.LookupCA(host)
+		if err != nil {
+			LogWarn("The CA for this host could not be determined from DNS.")
+			LogWarn("You can manually specify the CA by running:")
+			LogWarn("")
+			LogWarn("\toinit add " + hostport + " [ca]")
+			return
+		}
+
+		ca = detected
+	}
+
+	// Try to contact CA, which returns the host CA public key to be added
+	// to the user's known_hosts file.
+	if res, err := liboinitca.NewClient(ca).GetHost(host); err != nil {
+		LogError("Could not contact CA, " + err.Error())
+		return
+	} else {
+		if err := sshutil.AddSSHKnownHost(host, port, res.PublicKey); err != nil {
+			LogWarn("Could not add public key to your known_hosts file.")
+
+			if newLine, err := sshutil.GenerateKnownHosts(host, port, res.PublicKey); err == nil {
+				LogWarn("Please add the following line by yourself:")
+				LogWarn("\t" + newLine)
+			}
+		}
+	}
+
+	// Add to users' hosts file.
+	if err := sshutil.AddHostUser(hostport, ca); err != nil {
+		LogError("Could not add host: " + err.Error())
+		return
+	} else {
+		LogSuccess(hostport + " was added.")
+	}
+
+	// Check if 'Match exec ...' block is present, and if not try to add it.
+	if added, err := sshutil.AddSSHMatchBlock(); err != nil {
+		LogWarn("Could not read or modify your OpenSSH config file.")
+		LogWarn("Please verify it contains the following lines:")
+		LogWarn("")
+
+		for _, line := range strings.Split(sshutil.GenerateMatchBlock(), "\n") {
+			LogWarn("\t" + line)
+		}
+	} else if added {
+		LogInfo("As this is your first time running oinit, your OpenSSH config file has")
+		LogInfo("been modified to invoke oinit when ssh'ing to hosts managed by it.")
+	}
 }
 
 func handleCommandDelete(args []string) {
-	// todo
+	if len(args) < 1 {
+		fmt.Print(USAGE)
+		return
+	}
+
+	hostport := args[0]
+
+	found, err := sshutil.DeleteHostUser(hostport)
+	if err != nil {
+		LogError("Could not delete host: " + err.Error())
+		return
+	}
+
+	if !found {
+		LogError(hostport + " is either not managed by oinit, or configured system-wide.")
+	} else {
+		LogSuccess(hostport + " was deleted.")
+	}
 }
 
 func handleCommandList() {
-	// todo
+	all, err := sshutil.GetManagedHosts()
+	if err != nil {
+		LogError("Could not load hosts: " + err.Error())
+		return
+	}
+
+	hosts := make([]string, 0, len(all))
+	for hostport := range all {
+		hosts = append(hosts, hostport)
+	}
+	sort.Strings(hosts)
+
+	LogInfo("The following hosts are managed by oinit:")
+
+	for _, host := range hosts {
+		fmt.Println("\t" + host)
+	}
 }
 
 func handleCommandMatch(args []string) {
 	// todo
-
-	os.Exit(1) // no match
 }
 
 func main() {
