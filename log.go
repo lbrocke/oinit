d405bde173fc51821b62e26849a9bb6de22e301a
Lukas Brocke <lukas@brocke.net>
2023-06-07 15:56:27 +0200

    log: Add Prompt function to write without line break
    Also use go-tty package to properly support all operating systems.

--- log.go
+++ log.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/mattn/go-tty"
 )
 
 type Color string
@@ -12,59 +14,81 @@ const (
 	SYMBOL_SUCCESS = "✔"
 	SYMBOL_WARN    = "!"
 	SYMBOL_INFO    = "i"
+	SYMBOL_NONE    = ""
 
-	COLOR_RED    = "\u001b[31m"
-	COLOR_GREEN  = "\u001b[32m"
-	COLOR_YELLOW = "\u001b[33m"
-	COLOR_BLUE   = "\u001b[34m"
-	COLOR_RESET  = "\u001b[0m"
+	COLOR_RED    Color = "\u001b[31m"
+	COLOR_GREEN  Color = "\u001b[32m"
+	COLOR_YELLOW Color = "\u001b[33m"
+	COLOR_BLUE   Color = "\u001b[34m"
+	COLOR_RESET  Color = "\u001b[0m"
 )
 
-func log(msg string, color string, symbol string, tty bool) {
-	var str string
-
+func log(msg string, color Color, symbol string, useTTY bool, lb bool) {
 	// See https://no-color.org
 	if os.Getenv("NO_COLOR") != "" {
-		str = symbol + " " + msg + "\n"
-	} else {
-		str = color + symbol + COLOR_RESET + " " + msg + "\n"
+		color = ""
 	}
 
-	if tty {
-		os.WriteFile("/dev/tty", []byte(str), 0644)
+	var prefix string
+	if symbol != "" {
+		prefix = fmt.Sprintf("%s%s%s ", color, symbol, COLOR_RESET)
+	}
+
+	str := prefix + msg
+	if lb {
+		str = str + "\n"
+	}
+
+	if useTTY {
+		if tty, err := tty.Open(); err == nil {
+			defer tty.Close()
+			tty.Output().WriteString(str)
+		}
 	} else {
 		fmt.Print(str)
 	}
 }
 
 func LogError(msg string) {
-	log(msg, COLOR_RED, SYMBOL_ERROR, false)
+	log(msg, COLOR_RED, SYMBOL_ERROR, false, true)
 }
 
 func LogErrorTTY(msg string) {
-	log(msg, COLOR_RED, SYMBOL_ERROR, true)
+	log(msg, COLOR_RED, SYMBOL_ERROR, true, true)
 }
 
 func LogSuccess(msg string) {
-	log(msg, COLOR_GREEN, SYMBOL_SUCCESS, false)
+	log(msg, COLOR_GREEN, SYMBOL_SUCCESS, false, true)
 }
 
 func LogSuccessTTY(msg string) {
-	log(msg, COLOR_GREEN, SYMBOL_SUCCESS, true)
+	log(msg, COLOR_GREEN, SYMBOL_SUCCESS, true, true)
 }
 
 func LogWarn(msg string) {
-	log(msg, COLOR_YELLOW, SYMBOL_WARN, false)
+	log(msg, COLOR_YELLOW, SYMBOL_WARN, false, true)
 }
 
 func LogWarnTTY(msg string) {
-	log(msg, COLOR_YELLOW, SYMBOL_WARN, true)
+	log(msg, COLOR_YELLOW, SYMBOL_WARN, true, true)
 }
 
 func LogInfo(msg string) {
-	log(msg, COLOR_BLUE, SYMBOL_INFO, false)
+	log(msg, COLOR_BLUE, SYMBOL_INFO, false, true)
 }
 
 func LogInfoTTY(msg string) {
-	log(msg, COLOR_BLUE, SYMBOL_INFO, true)
+	log(msg, COLOR_BLUE, SYMBOL_INFO, true, true)
+}
+
+func Log(msg string) {
+	log(msg, COLOR_RESET, SYMBOL_NONE, false, true)
+}
+
+func LogTTY(msg string) {
+	log(msg, COLOR_RESET, SYMBOL_NONE, true, true)
+}
+
+func PromptTTY(msg string) {
+	log(msg, COLOR_RESET, SYMBOL_NONE, true, false)
 }
