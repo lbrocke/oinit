fef679562bf3fcd03f8ca9c62d9784b83f1fdbdf
Lukas Brocke <lukas@brocke.net>
2023-06-27 18:42:12 +0200

    cmd/oinit-switch: Take username instead of token as argument

--- cmd/oinit-switch/oinit-switch.go
+++ cmd/oinit-switch/oinit-switch.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"oinit/pkg/libmotleycue"
 	"oinit/pkg/passwd"
 	"os"
 	"os/exec"
 	"os/user"
 	"strconv"
+	"strings"
 	"syscall"
 
 	"oinit/pkg/log"
@@ -17,130 +16,122 @@ const (
 	OINIT_USER    = "oinit"
 	DEFAULT_SHELL = "/bin/sh"
 
-	// These messages should never be seen by normal users, when the system is
-	// set up correctly. Curious users may however execute this program manually.
-	ERR_WRONG_USER   = "Program must be executed by oinit user."
-	ERR_NO_ROOT_PERM = "Program must be executed with root permissions."
-	ERR_INVALID_ARGS = "Invalid arguments."
-
-	// These messages could be displayed to the user.
-	ERR_DEPLOY_FAILED = "Deploying your user account failed, the server responded: %s"
-	ERR_SUSPENDED     = "Your user account is not deployed or suspended."
-	ERR_DROP_PRIVS    = "Could not switch to your user account."
+	ERR_MISSING_PERMISSION = "Missing permissions."
+	ERR_NOT_ALLOWED        = "This is not allowed."
+	ERR_DROP_PRIVILEGES    = "Could not switch user."
 )
-
-// assertUser makes sure that this program is executed by OINIT_USER but with
-// root permissions. Otherwise it fails with exit code 1.
-func assertUser() {
-	// Program must be executed with root permissions
-	if os.Geteuid() != 0 {
-		log.LogFatal(ERR_NO_ROOT_PERM)
-	}
-
-	user, err := user.Lookup(OINIT_USER)
-	if err != nil {
-		log.LogFatal(ERR_WRONG_USER)
-	}
-
-	uid, err := strconv.Atoi(user.Uid)
-	if err != nil {
-		log.LogFatal(ERR_WRONG_USER)
-	}
-
-	// Program must be executed by OINIT_USER (or root)
-	if os.Getuid() != uid || os.Getuid() != 0 {
-		log.LogFatal(ERR_WRONG_USER)
-	}
-}
 
 // dropPrivileges uses setuid/setgid to drop all root privileges. Any error
 // will immediately exit this program.
-// Returns the user that was switched to.
-func dropPrivileges(username string) *user.User {
-	su, err := user.Lookup(username)
-	if err != nil {
-		log.LogFatal(ERR_DROP_PRIVS)
-	}
-
-	var uid, gid int
-	if uid, err = strconv.Atoi(su.Uid); err != nil {
-		log.LogFatal(ERR_DROP_PRIVS)
-	}
-	if gid, err = strconv.Atoi(su.Gid); err != nil {
-		log.LogFatal(ERR_DROP_PRIVS)
-	}
-
+func dropPrivileges(uid, gid int) {
 	if err := syscall.Setgroups([]int{}); err != nil {
-		log.LogFatal(ERR_DROP_PRIVS)
+		log.LogFatal(ERR_DROP_PRIVILEGES)
 	}
 
 	if err := syscall.Setgid(gid); err != nil || uid == 0 {
-		log.LogFatal(ERR_DROP_PRIVS)
+		log.LogFatal(ERR_DROP_PRIVILEGES)
 	}
 
 	if err := syscall.Setuid(uid); err != nil || gid == 0 {
-		log.LogFatal(ERR_DROP_PRIVS)
+		log.LogFatal(ERR_DROP_PRIVILEGES)
 	}
-
-	os.Chdir(su.HomeDir)
-
-	return su
 }
 
-// Usage: "oinit-switch <token> <url>" with
-//   - token: JWT access token
-//   - url:   HTTPS URL of motley_cue
-func main() {
-	// Make sure that this program is executed by correct user (uid) but with
-	// root permissions (euid) due to sticky bit being set
-	assertUser()
-
-	if len(os.Args) != 3 {
-		log.LogFatal(ERR_INVALID_ARGS)
-	}
-
-	// The token is not verified any further. The signer key is unknown anyway,
-	// therefore decoding without verification would only give an information
-	// about whether the given string is a valid JWT. An expiry check is done
-	// by motley_cue anyway.
-	// Also, parsing the JWT (which runs unknown code) is dangerous when this
-	// program is executed with root permissions.
-	token := os.Args[1]
-	url := os.Args[2]
-
-	res, err := libmotleycue.NewClient(url).GetUserDeploy(token)
+// getUser returns the uid and gid for the given username. If the user doesn't
+// exist, getUser exists immediately with code 1.
+func getUser(name string) (int, int) {
+	u, err := user.Lookup(name)
 	if err != nil {
-		log.LogFatal(fmt.Sprintf(ERR_DEPLOY_FAILED, err))
-	} else if res.State != libmotleycue.StateDeployed {
-		log.LogFatal(ERR_SUSPENDED)
+		log.LogFatal(ERR_DROP_PRIVILEGES)
 	}
 
-	// Drop root privileges using setgid/setgid system calls.
-	user := dropPrivileges(res.Credentials.SSHUser)
-	shell := passwd.Shell(res.Credentials.SSHUser, DEFAULT_SHELL)
+	var uid, gid int
+	if uid, err = strconv.Atoi(u.Uid); err != nil {
+		log.LogFatal(ERR_DROP_PRIVILEGES)
+	}
+	if gid, err = strconv.Atoi(u.Gid); err != nil {
+		log.LogFatal(ERR_DROP_PRIVILEGES)
+	}
 
-	sshCmd := os.Getenv("SSH_ORIGINAL_COMMAND")
-	hasSshCmd := sshCmd != ""
+	return uid, gid
+}
+
+// Returns a list of all environment variables (from os.Environ()) that start
+// with LC_ or lc_
+func getLocaleEnvs() []string {
+	var envs []string
+
+	for _, env := range os.Environ() {
+		name, _, ok := strings.Cut(env, "=")
+
+		if ok && strings.HasPrefix(strings.ToUpper(name), "LC_") {
+			envs = append(envs, env)
+		}
+	}
+
+	return envs
+}
+
+// Usage: "oinit-switch <target>" with target begin the name of user to switch
+// to.
+func main() {
+	// root permissions are required in any case.
+	// Either the process owner is root or process was started with root
+	// permissions (via setuid bit on executable).
+	if os.Geteuid() != 0 {
+		log.LogFatal(ERR_MISSING_PERMISSION)
+	}
+
+	if len(os.Args) != 2 {
+		log.LogFatal(ERR_NOT_ALLOWED)
+	}
+
+	target := os.Args[1]
+
+	targetUid, targetGid := getUser(target)
+
+	// Switching to root is not allowed.
+	if targetUid == 0 || targetGid == 0 {
+		log.LogFatal(ERR_NOT_ALLOWED)
+	}
+
+	oinitUid, _ := getUser(OINIT_USER)
+
+	if os.Getuid() == 0 || os.Getuid() == oinitUid || os.Getuid() == targetUid {
+		// Only the "oinit" user is allowed to switch to anyone (except to root).
+		// Also allow a non-root user to switch to himself/herself.
+		dropPrivileges(targetUid, targetGid)
+		goto UNPRIVILEGED
+	}
+
+	log.LogFatal(ERR_NOT_ALLOWED)
+
+UNPRIVILEGED:
+
+	// Double check b/c paranoia
+	if os.Getuid() != targetUid || os.Getgid() != targetGid {
+		log.LogFatal(ERR_DROP_PRIVILEGES)
+	}
+
+	shell := passwd.Shell(target, DEFAULT_SHELL)
 
 	var cmd *exec.Cmd
 
 	// Check if SSH_ORIGINAL_COMMAND is set, if yes run command instead of
 	// spawning an interactive shell.
-	if hasSshCmd {
+	if sshCmd, ok := os.LookupEnv("SSH_ORIGINAL_COMMAND"); ok {
 		// Use user's shell to execute command so that nice-to-have's like glob
 		// expansions work.
 		cmd = exec.Command(shell, "-c", sshCmd)
 	} else {
 		cmd = exec.Command(shell, "-il")
+
+		// Only set stdin if interactive login shell
+		cmd.Stdin = os.Stdin
 	}
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	// Only set stdin if interactive login shell
-	if !hasSshCmd {
-		cmd.Stdin = os.Stdin
-	}
 
 	// Override environment variables as setuid/setgid and exec.Command() don't
 	// do this automatically. Only certain required or useful variables
@@ -151,17 +142,22 @@ func main() {
 	//   password entry. $PATH defaults to [..] for normal users, and to [..]
 	//   for root, if not otherwise configured.
 
-	cmd.Env = []string{
-		"USER=" + user.Username,
-		"LOGNAME=" + user.Username,
-		"HOME=" + user.HomeDir,
-		"PWD=" + user.HomeDir,
+	targetUser, _ := user.Lookup(target)
+
+	cmd.Env = append([]string{
+		"USER=" + targetUser.Username,
+		"LOGNAME=" + targetUser.Username,
+		"HOME=" + targetUser.HomeDir,
+		"PWD=" + targetUser.HomeDir,
 		"SHELL=" + shell,
 		"TERM=" + os.Getenv("TERM"),
 		// Set by OpenSSH:
 		"SSH_CONNECTION=" + os.Getenv("SSH_CONNECTION"),
 		"SSH_CLIENT=" + os.Getenv("SSH_CLIENT"),
-	}
+	}, getLocaleEnvs()...)
+
+	// Required, as setting PWD isn't enough
+	os.Chdir(targetUser.HomeDir)
 
 	if err := cmd.Run(); err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok {
