46322a3b9d2d7df19889c387d97f578aed1800b0
Lukas Brocke <lukas@brocke.net>
2023-08-13 17:44:14 +0200

    scripts: Add notice for admin to modify /etc/pam.d/su

--- scripts/oinit-openssh-preremove.sh
+++ scripts/oinit-openssh-preremove.sh
@@ -6,3 +6,6 @@ set -eu
 if getent passwd oinit >/dev/null; then
     userdel oinit > /dev/null
 fi
+
+echo "Make sure to remove the two lines allowing 'oinit' to switch users without a"
+echo "password from '/etc/pam.d/su'!"
