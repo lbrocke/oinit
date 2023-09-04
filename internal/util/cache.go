11c73fadcc3f500c157b20c671e40be087c91e87
Lukas Brocke <lukas@brocke.net>
2023-09-04 23:22:37 +0200

    internal/util: Add tests and documentation

--- internal/util/cache.go
+++ internal/util/cache.go
@@ -4,6 +4,19 @@ import (
 	"time"
 )
 
+// NewTimedCache creates a new instance of a TimedCache with specified key and
+// value types and returns a pointer to it.
+//
+// The TimedCache is a data structure that allows you to store key-value pairs
+// with expiration times. You can specify the types of keys and values using
+// the 'K' and 'E' type parameters. The TimedCache is initialized as an empty
+// cache, ready to be used for caching values with specified expiration times.
+//
+// Example:
+//
+//	cache := NewTimedCache[string, int]()
+//	// Creates a new TimedCache instance for string keys and int values.
+//	// It is initially empty and ready to be used for caching values.
 func NewTimedCache[K comparable, E any]() *TimedCache[K, E] {
 	return &TimedCache[K, E]{
 		entries: make(map[K]timedCacheEntry[E]),
@@ -19,6 +32,22 @@ type timedCacheEntry[E any] struct {
 	expires time.Time
 }
 
+// Get retrieves a value associated with a specified key from the TimedCache.
+//
+// If the key exists and has not expired, this function returns the value
+// associated with the key and a boolean value 'true' to indicate success.
+//
+// If the key does not exist or has expired, it returns the zero value of the
+// cache's value type and 'false'.
+//
+// Example:
+//
+//	cache := NewTimedCache[string, int]()
+//	cache.Set("key1", 42, 10*time.Second)
+//	value, exists := cache.Get("key1")
+//	// 'value' will be 42, and 'exists' will be 'true' within the specified
+//	// duration of 10 seconds, otherwise 'value' will be the zero value of int
+//	// (0) and 'exists' will be 'false'.
 func (c TimedCache[K, E]) Get(key K) (E, bool) {
 	var content E
 
@@ -36,6 +65,19 @@ func (c TimedCache[K, E]) Get(key K) (E, bool) {
 	return entry.content, true
 }
 
+// Set adds a key-value pair to the TimedCache with a specified expiration time.
+//
+// It associates the given key of type K with the provided value of type E and
+// sets an expiration time based on the specified duration. After this duration
+// has passed, attempting to retrieve the value using the 'Get' method will
+// return 'false' to indicate that the key has expired.
+//
+// Example:
+//
+//	cache := NewTimedCache[string, int]()
+//	cache.Set("key1", 42, 10*time.Second)
+//	// The value 42 is associated with "key1" and will be valid for 10 seconds.
+//	// After that, using 'cache.Get("key1")' will return 'false'.
 func (c TimedCache[K, E]) Set(key K, content E, duration time.Duration) {
 	c.entries[key] = timedCacheEntry[E]{
 		content: content,
--- internal/util/util.go
+++ internal/util/util.go
@@ -1,3 +1,4 @@
+// Package util provides utility functions for various common tasks.
 package util
 
 import (
@@ -5,12 +6,20 @@ import (
 	"strings"
 )
 
-// matchesHost determines whether given host and port match host2 and port2.
-// host2 may be a wildcard domain in the form of
+// MatchesHost determines whether the given host and port match host2 and port2.
+// The host2 parameter may contain wildcard domains in the form of "*.example.com",
+// which match any subdomain of example.com, but not example.com itself.
 //
-//	*.example.com
+// Example usage:
 //
-// which matches any subdomain of example.com, but not example.com itself.
+//	result := MatchesHost("example.com", "22", "example.com", "22")
+//	// result will be true
+//
+//	result := MatchesHost("sub.example.com", "22", "*.example.com", "22")
+//	// result will be true
+//
+//	result := MatchesHost("example.com", "22", "*.example.com", "22")
+//	// result will be false
 func MatchesHost(host string, port string, host2 string, port2 string) bool {
 	if strings.HasPrefix(host2, "*.") {
 		root, _ := strings.CutPrefix(host2, "*.")
@@ -21,8 +30,14 @@ func MatchesHost(host string, port string, host2 string, port2 string) bool {
 	}
 }
 
-// Getenvs is equal to os.Getenv() but for multiple keys. It returns the first
-// non-empty value or an empty string, if none is set.
+// Getenvs retrieves environment variable values for multiple keys and returns
+// the first non-empty value found, or an empty string if none of the keys are set.
+//
+// Example usage:
+//
+//	value := Getenvs("MY_VAR", "ANOTHER_VAR", "YET_ANOTHER_VAR")
+//	// value will contain the value of the first set environment variable among "MY_VAR", "ANOTHER_VAR", and "YET_ANOTHER_VAR",
+//	// or an empty string if none of them are set.
 func Getenvs(keys ...string) string {
 	for _, key := range keys {
 		if val := os.Getenv(key); val != "" {
